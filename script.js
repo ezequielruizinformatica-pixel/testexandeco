7f2818d92ca051c0a33afccb31a9921f497be4a1
ezequielruizinformatica-pixel <ezequielruizinformatica@gmail.com>
2026-03-16 16:17:40 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,60 @@
+let alunos = [];
 
+function adicionarAluno(){
+
+let nome = document.getElementById("nomeAluno").value;
+
+if(nome === "") return;
+
+alunos.push({
+nome:nome,
+pontos:0
+});
+
+document.getElementById("nomeAluno").value="";
+
+render();
+}
+
+function adicionarPonto(index){
+
+alunos[index].pontos++;
+
+render();
+
+}
+
+function removerPonto(index){
+
+alunos[index].pontos--;
+
+render();
+
+}
+
+function render(){
+
+let tabela = document.getElementById("listaAlunos");
+
+tabela.innerHTML="";
+
+alunos.sort((a,b)=> b.pontos - a.pontos);
+
+alunos.forEach((aluno,index)=>{
+
+tabela.innerHTML += `
+<tr>
+<td>${aluno.nome}</td>
+<td>${aluno.pontos}</td>
+
+<td>
+<button onclick="adicionarPonto(${index})">+1</button>
+<button onclick="removerPonto(${index})">-1</button>
+</td>
+
+</tr>
+`;
+
+});
+
+}
